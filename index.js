c042afcd7f16fe3e0eccceadf7cfb9f873c731e0
Joe Ganobsik <jganobsik@gmail.com>
2019-03-19 22:04:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ const fareDoubler = createFareMultiplier(2);
 const fareTripler = createFareMultiplier(3);
 
 
-function selectDifferentDrivers(array, func) {
+function fetchSpecifiedDrivers(array, func) {
  return func(array);
 }
